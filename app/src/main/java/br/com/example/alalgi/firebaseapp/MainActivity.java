673382649aad17fa3c1758fa7fdaeab96f9656ef
gilmar <jiraspiom@yahoo.com.br>
2018-10-23 20:29:46 -0300

    Referencia firebase add

--- app/src/main/java/br/com/example/alalgi/firebaseapp/MainActivity.java
+++ app/src/main/java/br/com/example/alalgi/firebaseapp/MainActivity.java
@@ -3,6 +3,8 @@ package br.com.example.alalgi.firebaseapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.FirebaseApp;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
